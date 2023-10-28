1cfe1272f5639815432340fa0482ac7e8aa4a4d1
LuongNguyen171 <luongnguyen170102@gmail.com>
2023-10-28 09:22:03 +0700

    update login google (90%)

---
+++ app/Http/Controllers/GoogleController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Socialite\Facades\Socialite;
+use Exception;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class GoogleController extends Controller
+{
+    //
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        try {
+
+            $user = Socialite::driver('google')->user();
+
+            $finduser = User::where('google_id', $user->id)->first();
+
+            if ($finduser) {
+
+                Auth::login($finduser);
+
+                return redirect()->intended('dashboard');
+            } else {
+                $newUser = User::updateOrCreate(['userEmail' => $user->email], [
+                    'userName' => $user->name,
+                    'google_id' => $user->id,
+                    'userPassword' => encrypt('123456dummy')
+                ]);
+
+                Auth::login($newUser);
+
+                return redirect()->intended('dashboard');
+            }
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+}
